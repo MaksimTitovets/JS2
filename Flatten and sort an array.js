0bd1f802e1141502e0d7dcc988c90a9d820c510a
Maksim Titovets <mks.titovets@gmail.com>
2023-06-13 03:03:53 +0400

    Update Flatten and sort an array.js

--- Flatten and sort an array.js
+++ Flatten and sort an array.js
@@ -7,3 +7,10 @@ Example:
 
 Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
 */
+
+function flattenAndSort(array) {
+  let arr = [];
+  for (let i = 0; i < array.length; i++) 
+    arr = arr.concat(array[i]);
+  return arr.sort((a, b) => a - b);
+}
